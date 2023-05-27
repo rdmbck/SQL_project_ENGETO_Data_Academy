903edcb245d562056d83629f8862c84679deee58
rdmbck <84859088+rdmbck@users.noreply.github.com>
2023-05-27 16:30:29 +0200

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -5,13 +5,14 @@
 */
 
 WITH quantity_of_food AS (
-	SELECT *
-	FROM t_rdm_bck_project_sql_primary_final
-	WHERE food_code = "114201" OR food_code = "111301" 
-	GROUP BY `year`, food_code, industry_branch_name  
-	)
-SELECT 	*,
-		round((average_industry_wage / average_price), 0) AS food_quantity
+SELECT *
+FROM t_rdm_bck_project_sql_primary_final
+WHERE food_code = "114201" OR food_code = "111301" 
+GROUP BY `year`, food_code, industry_branch_name  
+)
+SELECT 	
+	*,
+	round((average_industry_wage / average_price), 0) AS food_quantity
 FROM quantity_of_food
 WHERE `year` = 2006 OR `year` = 2018 
 GROUP BY `year`, food_code, industry_branch_name 
